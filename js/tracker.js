06d743486937da0fe68bca98b82a6da6fc0f1b7f
Shawn Flesher <sflesh2@uic.edu>
2020-04-19 20:15:19 +0000

    added a changeScene function. fixed the table formatting

--- js/tracker.js
+++ js/tracker.js
@@ -19,9 +19,22 @@ document.querySelector("#refresh").addEventListener("click", (e)=>{
   refresh();
 }); 
 
-
+let currScene = "Home"; //always start on the home screen
+let changeScene = (e)=>{
+    //move the app to the data page
+        // <a class="mdc-list-item mdc-list-item--activated" href="#" id="home" aria-current="page">
+        //  <i class="material-icons mdc-list-item__graphic" aria-hidden="true">queue</i>
+        document.querySelector("#" + currScene).style = "display: none;";
+        document.querySelector("#"+e).style = "display: block";
+    
+        let remove = document.querySelector("#"+currScene.toLowerCase());    
+        remove.classList.remove("mdc-list-item--activated");
+       
+        let add = document.querySelector("#"+e.toLowerCase());
+        add.classList.add("mdc-list-item--activated");
+        currScene = e;
+}
 let selection = null;
-
 let hover=(e)=>{
     if(selection == null || selection != e){
          e.style.backgroundColor = "lightCyan";
@@ -72,12 +85,16 @@ document.querySelector(".remove-button").addEventListener("click", (e)=>{
             trackedNations.splice(trackedNations.indexOf(selection.id),1); 
             document.querySelector(".country-list").removeChild(selection);
             selection = null;
-            console.log(trackedNations);
         }
 });
 
 document.querySelector(".add-button").addEventListener("click", (e)=>{
-    let s = document.querySelector("#country").value;
+    let list = document.querySelector("#selector"); 
+    let s = list.value;
+    if(s == ""){
+        alert("Please Select a value");
+        return;
+    }
     let clone=document.querySelector(".template-list-item").cloneNode(true); 
     clone.classList.remove("template-list-item");
     clone.textContent = s;
@@ -89,8 +106,101 @@ document.querySelector(".add-button").addEventListener("click", (e)=>{
         trackedNations.push(s);
         
     }
+    list.value = "";
+    
     
 });
+
+document.querySelector(".clear-button").addEventListener("click", (e)=>{
+    trackedNations = []; //remove all the items in tracked nations 
+    
+    let parent = document.querySelector(".country-list");
+    let children = parent.children; 
+    let removeList = [];
+    for(let child of children){
+        if(child.classList.contains("template-list-item") == false){
+            removeList.push(child);
+        }
+    }
+    
+    for(let r of removeList){
+        parent.removeChild(r);
+    }
+});
+
+document.querySelector(".data-button").addEventListener("click", (e)=>{
+    if(trackedNations.length === 0){
+        alert("You must add countries in order to plot data");
+    }else{
+        let table = document.querySelector(".mdc-data-table__content");
+        
+        
+        let children = table.children; 
+        let removeList = [];
+        for(let child of children){
+            if(child.classList.contains("template-row") == false){
+                removeList.push(child);
+            }
+        }
+
+        for(let r of removeList){
+            table.removeChild(r);
+        }
+        
+        
+       for(let item of trackedNations){
+            let row = document.querySelector(".template-row").cloneNode(true);
+            row.classList.remove("template-row");
+            let i = allData[item]; 
+            let entryData = i[i.length-1];
+            
+            
+            let country = document.querySelector(".template-cell").cloneNode(true);
+            country.classList.remove("template-cell"); 
+            
+            let confirmed = country.cloneNode(true); 
+            let death = country.cloneNode(true); 
+            let recovered = country.cloneNode(true); 
+            
+            country.textContent = item; 
+            confirmed.textContent = entryData["confirmed"]; 
+            death.textContent = entryData["deaths"];
+            recovered.textContent=entryData["recovered"];
+            row.appendChild(country); 
+            row.appendChild(confirmed); 
+            row.appendChild(death); 
+            row.appendChild(recovered); 
+            
+            table.appendChild(row);
+           
+        }
+        
+        changeScene("Data");
+    }
+});
+
+document.querySelectorAll("aside.mdc-drawer a.mdc-list-item").forEach(item=>{
+       
+       item.addEventListener("click", even => {
+           let target = item.id;
+           
+           let pages = document.querySelectorAll(".page"); 
+           for(page of pages){
+               page.style = "display:none;"
+           }
+           
+           if(target == "home"){
+              changeScene("Home");
+           }else if(target == "data"){
+               changeScene("Data"); 
+           }else if(target == "add"){
+               changeScene("Add");
+           }else if(target == "charts"){
+               changeScene("Charts");
+           }
+           drawer.open = false;
+       });
+    });
 
 let allData = null;
 fetch("https://pomber.github.io/covid19/timeseries.json")
